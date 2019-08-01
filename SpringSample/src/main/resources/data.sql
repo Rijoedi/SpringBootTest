9baa139dff53ae6b4067a0cd8e70985225f1ce8e
sugie <sugie@192.168.0.7>
2019-08-01 23:42:12 +0900

    first spring boot

---
+++ SpringSample/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+CREATE TABLE employee(
+	employee_id INT(10),
+	employee_name VARCHAR(50),
+	age INT(10)
+);
---
+++ SpringSample/src/main/resources/schema.sql
@@ -0,0 +1,1 @@
+INSERT INTO EMPLOYEE　(employee_id,employee_name,age)VALUES(1,'sugie',23);
